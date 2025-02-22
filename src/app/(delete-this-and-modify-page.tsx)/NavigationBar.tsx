cd43d66219db13a1574be41ba4998df285a878fb
Siddhartha Maity <siddharthamaity1993@gmail.com>
2025-02-23 00:04:50 +0530

    feat: Add SVG icons for Next.js, Vercel, and other components; update HomePage and NavigationBar to use local SVGs

--- src/app/(delete-this-and-modify-page.tsx)/NavigationBar.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/NavigationBar.tsx
@@ -5,7 +5,7 @@ import ThemeSwitch from '@/app/(delete-this-and-modify-page.tsx)/ThemeSwitch';
 
 const NavigationBar = () => {
     return (
-        <div className='mx-auto mt-3 flex w-full max-w-7xl flex-col-reverse items-center justify-between gap-6 px-3 sm:flex-row sm:px-0 lg:mt-6'>
+        <div className='mx-auto mt-3 flex w-full max-w-7xl flex-col-reverse items-center justify-end gap-6 px-3 sm:flex-row sm:px-0 lg:mt-6'>
             <div className='flex w-full justify-between gap-6 sm:w-auto sm:items-center'>
                 <ThemeSwitch />
                 <Link href='https://github.com/SiddharthaMaity/nextjs-15-starter-shadcn' target='_blank'>
