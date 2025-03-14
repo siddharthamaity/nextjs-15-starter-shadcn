e079512f898b637d1f4429b9ecf57054dff85e5c
Jesus Lopez <jesus@ascend.travel>
2025-03-14 15:45:44 -0300

    feat: Integrate LuckyOrange tracking script into Analytics component
    This commit adds a new LuckyOrange component that includes a tracking script for user analytics. The Analytics component is updated to render both VercelAnalytics and LuckyOrange, enhancing the application's tracking capabilities.

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,7 +1,26 @@
 'use client';
 
+import Script from 'next/script';
+
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
+function LuckyOrange() {
+    return (
+        <Script
+            id='lucky-orange'
+            strategy='afterInteractive'
+            src='https://tools.luckyorange.com/core/lo.js?site-id=ddfef143'
+            async
+            defer
+        />
+    );
+}
+
 export function Analytics() {
-    return <VercelAnalytics />;
+    return (
+        <>
+            <VercelAnalytics />
+            <LuckyOrange />
+        </>
+    );
 }
